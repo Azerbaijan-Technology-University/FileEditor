ca68a456a469f18e8c3cb1ac526363a1407ab6b7
nullshii <60722967+nullshii@users.noreply.github.com>
2026-05-16 19:19:02 +0400

    Removed unimplemented commands

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from cli_engine import CliEngine
-from commands.change_directory import ChangeDirectoryCommand
 from commands.clear import ClearCommand
 from commands.delete import DeleteCommand
 from commands.edit import EditCommand
 from commands.exit import ExitCommand
 from commands.help import HelpCommand
-from commands.info import InfoCommand
 from commands.list import ListCommand
 from commands.rename import RenameCommand
 
@@ -18,9 +16,7 @@ def main() -> None:
         .add_command(RenameCommand)
         .add_command(DeleteCommand)
         .add_command(ListCommand)
-        .add_command(ChangeDirectoryCommand)
         .add_command(ClearCommand)
-        .add_command(InfoCommand)
         .add_command(ExitCommand)
     )
     cli_engine.run()
